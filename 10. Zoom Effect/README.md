8acc184d31ac07a5fef7ad28083c412f499cd9c1
C.D. MacEachern <craigmaceachern@fastmail.com>
2020-09-24 10:57:22 -0400

    10. Zoom Effect done.

--- 10. Zoom Effect/README.md
+++ 10. Zoom Effect/README.md
@@ -1,8 +1,9 @@
-#
+# 10. Zoom Effect
 
+Load a picture that zooms in when you hover mouse over it.
 
 Requirements:
 
-*
-*
-*
+* Centered picture.
+* Capturing and handling mouse events using JavaScript
+* Transforming a DOM element with a callback.
--- 10. Zoom Effect/script.js
+++ 10. Zoom Effect/script.js
@@ -1,17 +1,21 @@
+// Parent div of image
 const container = document.getElementById("container");
+// Actual image element
 const img = document.querySelector("img");
 
+// Given "event" 'x' and 'y' values use them to transform the image
+// by scaling up (i.e., zooming in to 200%) current mouse location
 container.addEventListener("mousemove", (e) => {
-    const x = e.clientX - e.target.offsetLeft;
-    const y = e.clientY - e.target.offsetTop;
+  const x = e.clientX - e.target.offsetLeft;
+  const y = e.clientY - e.target.offsetTop;
 
-    console.log(x, y);
+  // Using newer Js template literal strings here to eval x and y
+  img.style.transformOrigin = `${x}px ${y}px`;
+  img.style.transform = "scale(2)";
+})
 
-    img.style.transformOrigin = `${x}px ${y}px`;
-    img.style.transform = "scale(2)";
-});
-
+// When mouse leaves, scale everything back to original form
 container.addEventListener("mouseleave", () => {
-    img.style.transformOrigin = "center center";
-    img.style.transform = "scale(1)";
-});
+  img.style.transformOrigin = "center center";
+  img.style.transform = "scale(1)";
+})
